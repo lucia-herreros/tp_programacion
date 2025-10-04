14f4420eca8e61f0b1c85703375786a2e13b59ff
Agustina Martin <agustmartin@uade.edu.ar>
2025-10-04 17:35:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,29 +4,32 @@ import pedidos
 
 def main():
     while True:
-        print("=== RESTAURANTE ===")
+        print("\n=== RESTAURANTE ===")
         print("1. Ver la carta")
         print("2. Hacer una reserva")
         print("3. Hacer un pedido")
-        print("4. Salir")
+        print("4. Ver reservas")
+        print("5. Salir")
         
         try: 
             opcion = int(input("Elija una opción: ").strip())
         except ValueError:
-            print("Por favor ingrese un número.\n")
+            print("⚠️ Por favor ingrese un número.\n")
             continue
         
         if opcion == 1:
             menu.mostrar_menu()
         elif opcion == 2:
-            print("Reserva")
+            reservas.hacer_reserva()
         elif opcion == 3:
-            print("Pedido")
+            pedidos.hacer_pedido()
         elif opcion == 4:
+            reservas.mostrar_reservas()
+        elif opcion == 5:
             print("¡Hasta pronto!")
             break
         else:
-            print("Opción inválida. Ingrese un número entre 1 y 4\n")
+            print("Opción inválida. Ingrese un número entre 1 y 5\n")
 
 if __name__ == "__main__":
     main()
